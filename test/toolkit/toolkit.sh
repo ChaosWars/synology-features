9a392ef32e224f073a85b46b8edd48b4a3590c56
Lawrence Lee <valheru.ashen.shugar@gmail.com>
2024-06-12 21:49:15 +0200

    Cleanup

--- test/toolkit/toolkit.sh
+++ test/toolkit/toolkit.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# This test file will be executed against one of the scenarios devcontainer.json test that
-# includes the 'color' feature with "greeting": "hello" option.
-
 set -eu
 
-# Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
 # Feature-specific tests
